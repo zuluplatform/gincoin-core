5c5d929ca3e35eed9065fe577b7a2a8a8aad1465
Dragos Badea <bedeabza@gmail.com>
2018-02-05 08:10:56 +0200

    replaced 'dash-tx' with 'gincoin-tx'

--- src/gincoin-tx.cpp
+++ src/gincoin-tx.cpp
@@ -52,10 +52,10 @@ static int AppInitRawTx(int argc, char* argv[])
     if (argc<2 || mapArgs.count("-?") || mapArgs.count("-h") || mapArgs.count("-help"))
     {
         // First part of help message is specific to this utility
-        std::string strUsage = _("Gincoin Core dash-tx utility version") + " " + FormatFullVersion() + "\n\n" +
+        std::string strUsage = _("Gincoin Core gincoin-tx utility version") + " " + FormatFullVersion() + "\n\n" +
             _("Usage:") + "\n" +
-              "  dash-tx [options] <hex-tx> [commands]  " + _("Update hex-encoded dash transaction") + "\n" +
-              "  dash-tx [options] -create [commands]   " + _("Create hex-encoded dash transaction") + "\n" +
+              "  gincoin-tx [options] <hex-tx> [commands]  " + _("Update hex-encoded dash transaction") + "\n" +
+              "  gincoin-tx [options] -create [commands]   " + _("Create hex-encoded dash transaction") + "\n" +
               "\n";
 
         fprintf(stdout, "%s", strUsage.c_str());
