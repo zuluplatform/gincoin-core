27253e6461e31e426f9b79fbd3a6425000068f5c
Dragos Badea <bedeabza@gmail.com>
2018-02-05 10:51:04 +0200

    replaced 'DashQt' with 'GincoinQt'

--- src/qt/paymentserver.cpp
+++ src/qt/paymentserver.cpp
@@ -82,7 +82,7 @@ namespace // Anon namespace
 //
 static QString ipcServerName()
 {
-    QString name("DashQt");
+    QString name("GincoinQt");
 
     // Append a simple hash of the datadir
     // Note that GetDataDir(true) returns a different path
