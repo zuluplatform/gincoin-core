5678e8cb18a1a979c1fb0f1156f3919e9fefb8fd
Dragos Badea <bedeabza@gmail.com>
2018-12-20 14:25:56 +0200

    changed BIP44 type to 2000 (Gincoin)

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -165,8 +165,9 @@ public:
         // Gincoin BIP32 prvkeys start with 'xprv' (Bitcoin defaults)
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
 
-        // Gincoin BIP44 coin type is '5'
-        nExtCoinType = 5;
+        // Gincoin BIP44 coin type
+        // https://github.com/satoshilabs/slips/blob/master/slip-0044.md
+        nExtCoinType = 2000;
 
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
 
